c8810eebd73f275627ee4b1e21e689f96f02cf5d
alexisBltz <almamanima@unsa.edu.pe>
2024-07-22 14:08:56 -0500

    Añadiendo modificaciones para para el redireccionamiento

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,11 +1,18 @@
 import {NavBar} from "../components/NavBar.jsx";
 import Footer from "../components/Footer.tsx"
 import {Outlet} from "react-router-dom";
+
+const initialNavigation= [
+    { name: 'Usuarios', to: '/', current: false },
+    { name: 'Servicios', to: '/paquetes', current: false },
+    { name: 'Paquetes', to: '/servicios', current: false },
+    { name: 'Ventas', to: '/nosotros', current: false },
+];
 export default function AdminLayout() {
     return (
         <>
 
-            <NavBar />
+            <NavBar initialNavigation={initialNavigation}/>
 
             <Outlet />
 
