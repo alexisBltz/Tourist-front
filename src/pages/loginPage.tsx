06e6ee4d46ab4f00dff064e70c3b74277fb531d6
Agustin Mamani <agusdmu1999@gmail.com>
2024-07-22 09:52:04 -0500

    fix error y estilos

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -14,7 +14,7 @@ export default function LoginPage (){
             await login(email, password);
             window.location.href = '/';
         } catch (error) {
-            setError('Login failed. Please try again.');
+            setError('Contraseña incorrecta. Por favor, intenta nuevamente.');
         }
     };
 
@@ -47,7 +47,7 @@ export default function LoginPage (){
                                     autoComplete="email"
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -72,11 +72,15 @@ export default function LoginPage (){
                                     value={password}
                                     onChange={(e) => setPassword(e.target.value)}
                                     autoComplete="current-password"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
-
+                        {error && (
+                            <div className="bg-gray-200 text-red-500 text-sm p-2 rounded-md">
+                                {error}
+                            </div>
+                        )}
                         <div>
                             <button
                                 type="submit"
