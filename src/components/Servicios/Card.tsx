0796556c3940281024c4c9a8bc806a288bd27c39
alexisBltz <almamanima@unsa.edu.pe>
2024-07-22 20:55:53 -0500

    Al hacer click a las cards nos dara mas informacion de ellas

--- src/components/Servicios/Card.tsx
+++ src/components/Servicios/Card.tsx
@@ -12,7 +12,7 @@ interface ServicioCardProps {
     fecha: string;
 }
 
-const ServicioCard: React.FC<ServicioCardProps> = ({ id, image, nombre, descripcion, fecha, costo, destino }) => {
+const ServicioCard: React.FC<ServicioCardProps> = ({ id, image, nombre, fecha, costo, destino }) => {
     const navigate = useNavigate();  // Crea una instancia de navigate
 
     const handleClick = () => {
@@ -25,13 +25,11 @@ const ServicioCard: React.FC<ServicioCardProps> = ({ id, image, nombre, descripc
             className="cursor-pointer"
             onClick={handleClick}  // Maneja el clic
         >
-        <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out transform hover:-translate-y-1">
-            <img className="w-full" src={image} alt={nombre} />
+        <div className="max-w-md rounded overflow-hidden shadow-lg m-4 bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out transform hover:-translate-y-1">
+            <img className="w-full h-64 object-cover" src={image} alt={nombre} />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{nombre}</div>
-                <p className="text-gray-700 text-base">{descripcion}</p>
                 <p className="text-gray-700 text-base">Fecha: {fecha}</p>
-                <p className="text-gray-700 text-base">Costo: S/. {costo}</p>
                 <p className="text-gray-700 text-base">Destino: {destino}</p>
             </div>
         </div>
